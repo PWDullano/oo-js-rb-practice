c38bf364db7636503a71d41a18e8f2acbfe63dcf
Jeff Dean <jeff@galvanize.it>
2015-02-23 08:16:38 -0700

    commented out solution

--- robots-ruby/spec/robot_spec.rb
+++ robots-ruby/spec/robot_spec.rb
@@ -15,33 +15,33 @@ RSpec.describe Robot do
     expect(robot2.battery).to eq(6)
   end
 
-  it "can move up" do
-    robot = Robot.new(8)
-    robot.move_up
-    expect(robot.position).to eq([0, 1])
-
-    robot.move_up
-    expect(robot.position).to eq([0, 2])
-  end
-
-  it "can move left" do
-    robot = Robot.new(8)
-    robot.move_left
-    expect(robot.position).to eq([-1, 0])
-
-    robot.move_left
-    expect(robot.position).to eq([-2, 0])
-  end
-
-  it "can record their history" do
-    robot = Robot.new(8)
-    expect(robot.history).to eq([])
-
-    robot.move_left
-    expect(robot.history).to eq([:left])
-
-    robot.move_up
-    expect(robot.history).to eq([:left, :up])
-  end
+  # it "can move up" do
+  #   robot = Robot.new(8)
+  #   robot.move_up
+  #   expect(robot.position).to eq([0, 1])
+  #
+  #   robot.move_up
+  #   expect(robot.position).to eq([0, 2])
+  # end
+  #
+  # it "can move left" do
+  #   robot = Robot.new(8)
+  #   robot.move_left
+  #   expect(robot.position).to eq([-1, 0])
+  #
+  #   robot.move_left
+  #   expect(robot.position).to eq([-2, 0])
+  # end
+  #
+  # it "can record their history" do
+  #   robot = Robot.new(8)
+  #   expect(robot.history).to eq([])
+  #
+  #   robot.move_left
+  #   expect(robot.history).to eq([:left])
+  #
+  #   robot.move_up
+  #   expect(robot.history).to eq([:left, :up])
+  # end
 
 end
