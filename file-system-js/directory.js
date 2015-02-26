b3705d4a9bb5137bdad59325bf076c1f7c72eead
Jeff Dean <jeff@galvanize.it>
2015-02-26 15:49:35 -0700

    updated file system tests

--- file-system-js/directory.js
+++ file-system-js/directory.js
@@ -1,42 +1,7 @@
 var Directory = function (name) {
-  // this.name = name;
-  // this._files = {};
 };
 
-// Directory.prototype.ls = function () {
-//   var result = [];
-//   for (var key in this._files) {
-//     if (this._files.hasOwnProperty(key)) {
-//       result.push(key);
-//     }
-//   }
-//   return result.sort();
-// };
-
-// Directory.prototype.write = function (name, content) {
-//   if(!this._files[name]) this._files[name] = {name: name};
-//   this._files[name].content = content;
-// };
-
-// Directory.prototype.cat = function (name) {
-//   return this._files[name].content;
-// };
-
-// Directory.prototype.mv = function (src, destination) {
-//   this._files[destination] = this._files[src];
-//   this._files[destination].name = destination;
-//   delete this._files[src];
-// };
-
-// Directory.prototype.cp = function (src, destination) {
-//   this._files[destination] = {
-//     name: this._files[src].name,
-//     content: this._files[src].content
-//   };
-// };
-
-// Directory.prototype.ln_s = function (src, destination) {
-//   this._files[destination] = this._files[src];
-// };
+Directory.prototype.ls = function () {
+};
 
 module.exports = Directory;
