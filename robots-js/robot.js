c386dd4b27cbd17113043f801107eee139a4e44b
Luke Bartel <bartelluke@gmail.com>
2015-03-03 21:00:07 -0700

    js spec and solution for robot

--- robots-js/robot.js
+++ robots-js/robot.js
@@ -1,5 +1,34 @@
-var Robot = function () {
+var position = [0,0]
+var charge = 0
+var history = []
 
+var Robot = function (startCharge) {
+  position = [0,0];
+  charge = startCharge;
+  history = [];
 };
+
+Robot.prototype.position = function() {
+  return position;
+};
+
+Robot.prototype.battery = function() {
+  return charge;
+};
+
+Robot.prototype.moveUp = function() {
+  position[1] ++;
+  history.push("up");
+};
+
+Robot.prototype.moveLeft = function() {
+  position[0] --;
+  history.push("left");
+};
+
+Robot.prototype.history = function() {
+  return history;
+};
+
 
 module.exports = Robot;
