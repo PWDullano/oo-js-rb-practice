3537cd0b8c1b158f15f0085b4bdd3c4b252d983c
Jeff Dean <jeff@galvanize.it>
2015-03-04 22:59:02 -0700

    prepped dmv spec for students

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,43 +1,8 @@
 var DMV = function (agents) {
-  // this._customersInLine = [];
-  // this._agents = agents;
-  // this._attending = {};
-  // agents.forEach(function (agent) {
-  //   this._attending[agent] = null;
-  // }.bind(this));
 };
 
-// DMV.prototype.customersInLine = function () {
-//   return this._customersInLine;
-// };
-//
-// DMV.prototype.enter = function (name) {
-//   this._customersInLine.push(name);
-// };
-//
-// DMV.prototype.currentCustomerFor = function (agent) {
-//   return this._attending[agent];
-// };
-//
-// DMV.prototype.nextCustomer = function () {
-//   for (var i = 0; i < this._agents.length; i++) {
-//     var agent = this._agents[i];
-//     if (this._attending[agent] === null) {
-//       this._attending[agent] = this._customersInLine.shift();
-//       return;
-//     }
-//   }
-// };
-//
-// DMV.prototype.resolve = function (customer) {
-//   for (var i = 0; i < this._agents.length; i++) {
-//     var agent = this._agents[i];
-//     var currentCustomer = this._attending[agent];
-//     if (customer === currentCustomer) {
-//       this._attending[agent] = null;
-//       return;
-//     }
-//   }
-// };
+DMV.prototype.customersInLine = function () {
+  return this._customersInLine;
+};
 
 module.exports = DMV;
--- dmv/spec/dmv_spec.js
+++ dmv/spec/dmv_spec.js
@@ -13,45 +13,45 @@ describe('DMV', ()=> {
     });
   });
 
-  it("sends customers to the next available agent", ()=> {
-    var dmv = new DMV(["Surly Suneel", "Angry Angelica", "Peeved Petra"]);
-
-    dmv.enter("Drivin' Dave");
-    dmv.enter("Speedy Spencer");
-    dmv.enter("Talkin' Tammy");
-
-    expect(dmv.currentCustomerFor("Surly Suneel")).toEqual(null);
-
-    dmv.nextCustomer();
-    expect(dmv.currentCustomerFor("Surly Suneel")).toEqual("Drivin' Dave");
-    expect(dmv.customersInLine()).toEqual(["Speedy Spencer", "Talkin' Tammy"]);
-
-    dmv.nextCustomer();
-    expect(dmv.currentCustomerFor("Surly Suneel")).toEqual("Drivin' Dave");
-    expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Speedy Spencer");
-    expect(dmv.customersInLine()).toEqual(["Talkin' Tammy"]);
-
-    dmv.nextCustomer();
-    expect(dmv.currentCustomerFor("Peeved Petra")).toEqual("Talkin' Tammy");
-    expect(dmv.customersInLine()).toEqual([]);
-  });
-
-  it("allows you to resolve an issue, which frees an agent up for the next customer", ()=> {
-    var dmv = new DMV(["Surly Suneel", "Angry Angelica"]);
-
-    dmv.enter("Skidding Skye");
-    dmv.enter("Drivin' Dave");
-    dmv.nextCustomer();
-    dmv.nextCustomer();
-    expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Drivin' Dave");
-
-    dmv.resolve("Drivin' Dave");
-    expect(dmv.currentCustomerFor("Angry Angelica")).toEqual(null);
-
-    dmv.enter("Texting Ty");
-    dmv.nextCustomer();
-    dmv.resolve("Drivin' Dave");
-    expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Texting Ty");
-  });
+  // it("sends customers to the next available agent", ()=> {
+  //   var dmv = new DMV(["Surly Suneel", "Angry Angelica", "Peeved Petra"]);
+  //
+  //   dmv.enter("Drivin' Dave");
+  //   dmv.enter("Speedy Spencer");
+  //   dmv.enter("Talkin' Tammy");
+  //
+  //   expect(dmv.currentCustomerFor("Surly Suneel")).toEqual(null);
+  //
+  //   dmv.nextCustomer();
+  //   expect(dmv.currentCustomerFor("Surly Suneel")).toEqual("Drivin' Dave");
+  //   expect(dmv.customersInLine()).toEqual(["Speedy Spencer", "Talkin' Tammy"]);
+  //
+  //   dmv.nextCustomer();
+  //   expect(dmv.currentCustomerFor("Surly Suneel")).toEqual("Drivin' Dave");
+  //   expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Speedy Spencer");
+  //   expect(dmv.customersInLine()).toEqual(["Talkin' Tammy"]);
+  //
+  //   dmv.nextCustomer();
+  //   expect(dmv.currentCustomerFor("Peeved Petra")).toEqual("Talkin' Tammy");
+  //   expect(dmv.customersInLine()).toEqual([]);
+  // });
+  //
+  // it("allows you to resolve an issue, which frees an agent up for the next customer", ()=> {
+  //   var dmv = new DMV(["Surly Suneel", "Angry Angelica"]);
+  //
+  //   dmv.enter("Skidding Skye");
+  //   dmv.enter("Drivin' Dave");
+  //   dmv.nextCustomer();
+  //   dmv.nextCustomer();
+  //   expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Drivin' Dave");
+  //
+  //   dmv.resolve("Drivin' Dave");
+  //   expect(dmv.currentCustomerFor("Angry Angelica")).toEqual(null);
+  //
+  //   dmv.enter("Texting Ty");
+  //   dmv.nextCustomer();
+  //   dmv.resolve("Drivin' Dave");
+  //   expect(dmv.currentCustomerFor("Angry Angelica")).toEqual("Texting Ty");
+  // });
 
 });
