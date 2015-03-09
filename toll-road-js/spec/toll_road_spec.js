8d774acee870f2a99559b8645bf2da5e19fb16c8
Jeff Dean <jeff@galvanize.it>
2015-03-09 09:38:16 -0600

    toll road

--- toll-road-js/spec/toll_road_spec.js
+++ toll-road-js/spec/toll_road_spec.js
@@ -23,32 +23,35 @@ describe("TollRoad", ()=>{
   //   expect(tollRoad.balance('ccc-ddd')).toEqual(7);
   // });
   //
+  // it("returns the dates that a given car was charged (most recent first)", ()=> {
+  //   var tollRoad = new TollRoad(6);
+  //
+  //   tollRoad.charge('aaa-bbb', '2015-12-12-08-22');
+  //   expect(tollRoad.history('aaa-bbb')).toEqual([
+  //     'Charged $6 on 2015-12-12-08-22'
+  //   ]);
+  //
+  //   tollRoad.charge('aaa-bbb', '2015-01-05-10-34');
+  //   expect(tollRoad.history('aaa-bbb')).toEqual([
+  //     'Charged $6 on 2015-01-05-10-34',
+  //     'Charged $6 on 2015-12-12-08-22',
+  //   ]);
+  // });
+  //
   // it("allows customers to pay", ()=> {
   //   var tollRoad = new TollRoad(5);
   //
   //   tollRoad.charge('aaa-bbb', '2015-12-12-08-22');
   //   tollRoad.charge('aaa-bbb', '2015-12-12-08-22');
+  //   tollRoad.charge('def-123', '2015-12-12-08-22');
   //
   //   tollRoad.pay('aaa-bbb', 7);
   //   expect(tollRoad.balance('aaa-bbb')).toEqual(3);
   //
   //   tollRoad.pay('aaa-bbb', 2);
   //   expect(tollRoad.balance('aaa-bbb')).toEqual(1);
-  // });
   //
-  // it("returns the dates that a given car was charged (most recent first)", ()=> {
-  //   var tollRoad = new TollRoad(6);
-  //
-  //   tollRoad.charge('aaa-bbb', '2015-12-12-08-22');
-  //   expect(tollRoad.history('aaa-bbb')).toEqual([
-  //     'Charged $0.06 on 2015-12-12-08-22'
-  //   ]);
-  //
-  //   tollRoad.charge('aaa-bbb', '2015-01-05-10-34');
-  //   expect(tollRoad.history('aaa-bbb')).toEqual([
-  //     'Charged $0.06 on 2015-01-05-10-34',
-  //     'Charged $0.06 on 2015-12-12-08-22',
-  //   ]);
+  //   expect(tollRoad.balance('def-123')).toEqual(5);
   // });
 
 });
