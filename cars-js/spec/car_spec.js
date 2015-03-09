e82ef25b0d9e4fd9831d20447416da801e7f3220
Jeff Dean <jeff@galvanize.it>
2015-03-09 10:28:10 -0600

    improve car spec to include a map

--- cars-js/spec/car_spec.js
+++ cars-js/spec/car_spec.js
@@ -15,32 +15,36 @@ describe('Car', function() {
     });
   });
 
-  // describe("#drive", function() {
-  //   it("uses gas", function() {
-  //     var car = new Car(10);
-  //     car.fill(10);
-  //     expect(car.gallons).toEqual(10);
-  //     car.drive(50);
-  //     expect(car.gallons).toEqual(5);
-  //   });
+  // it("uses gas when driving", function() {
+  //   // formula for how gas used is miles / mpg
+  //   var car = new Car(10);
+  //   car.fill(10);
+  //   expect(car.gallons).toEqual(10);
+  //   car.drive(50);
+  //   expect(car.gallons).toEqual(5);
+  // });
   //
-  //   // it("increments the odometer", function() {
-  //   //   var car = new Car(10);
-  //   //   expect(car.odometer).toEqual(0);
-  //   //   car.drive(50);
-  //   //   expect(car.odometer).toEqual(50);
-  //   //   car.drive(25);
-  //   //   expect(car.odometer).toEqual(75);
-  //   // });
-  //   //
-  //   // it("records trips", function() {
-  //   //   var car = new Car(10);
-  //   //   expect(car.trips).toEqual([]);
-  //   //   car.drive(50);
-  //   //   expect(car.trips).toEqual([50]);
-  //   //   car.drive(25);
-  //   //   expect(car.trips).toEqual([50, 25]);
-  //   // });
+  // it("increments the odometer when driving", function() {
+  //   var car = new Car(10);
+  //   expect(car.odometer).toEqual(0);
+  //   car.drive(50);
+  //   expect(car.odometer).toEqual(50);
+  //   car.drive(25);
+  //   expect(car.odometer).toEqual(75);
+  // });
+  //
+  // it("records trips", function() {
+  //   var car = new Car(10);
+  //   expect(car.trips).toEqual([]);
+  //   car.drive(50);
+  //   expect(car.trips).toEqual([
+  //     '50 miles'
+  //   ]);
+  //   car.drive(25);
+  //   expect(car.trips).toEqual([
+  //     '50 miles',
+  //     '25 miles',
+  //   ]);
   // });
 
 });
--- cars-ruby/spec/car_spec.rb
+++ cars-ruby/spec/car_spec.rb
@@ -3,50 +3,51 @@ require_relative '../car'
 
 RSpec.describe Car do
 
-  describe "#fill" do
-    it "gives the car gas" do
-      car = Car.new(10)
-      expect(car.gallons).to eq(0)
+  it "can be filled with gas" do
+    car = Car.new(10)
+    expect(car.gallons).to eq(0)
 
-      car.fill(5)
-      expect(car.gallons).to eq(5)
+    car.fill(5)
+    expect(car.gallons).to eq(5)
 
-      car.fill(5)
-      expect(car.gallons).to eq(10)
-    end
+    car.fill(5)
+    expect(car.gallons).to eq(10)
   end
 
-  describe "#drive" do
-    it "uses gas" do
-      car = Car.new(10)
-      car.fill(10)
-      expect(car.gallons).to eq(10)
-
-      car.drive(50)
-      expect(car.gallons).to eq(5)
-    end
-
-    it "increments the odometer" do
-      car = Car.new(10)
-      expect(car.odometer).to eq(0)
-
-      car.drive(50)
-      expect(car.odometer).to eq(50)
-
-      car.drive(25)
-      expect(car.odometer).to eq(75)
-    end
-
-    it "records trips" do
-      car = Car.new(10)
-      expect(car.trips).to eq([])
-
-      car.drive(50)
-      expect(car.trips).to eq([50])
-
-      car.drive(25)
-      expect(car.trips).to eq([50, 25])
-    end
-  end
+  # it "uses gas when driven" do
+  #   car = Car.new(10)
+  #   car.fill(10)
+  #   expect(car.gallons).to eq(10)
+  #
+  #   car.drive(50)
+  #   expect(car.gallons).to eq(5)
+  # end
+  #
+  # it "increments the odometer when driven" do
+  #   car = Car.new(10)
+  #   expect(car.odometer).to eq(0)
+  #
+  #   car.drive(50)
+  #   expect(car.odometer).to eq(50)
+  #
+  #   car.drive(25)
+  #   expect(car.odometer).to eq(75)
+  # end
+  #
+  # it "records trips" do
+  #   car = Car.new(10)
+  #   expect(car.trips).to eq([])
+  #
+  #   car.drive(50)
+  #   expect(car.trips).to eq([
+  #     "50 miles"
+  #   ])
+  #
+  #   car.drive(25)
+  #   expect(car.trips).to eq([
+  #     "50 miles",
+  #     "25 miles",
+  #   ])
+  # end
 
 end
